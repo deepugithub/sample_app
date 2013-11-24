54caab253226153d26dafbd2474aa867b275b6bf
NCI <nci@nci-VirtualBox.(none)>
2013-11-24 09:58:40 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '2a258579710cfb246d162c58aae2090fa4f7f2e67f633e9ec747bdd71f193896d05c91f7944468c8f99b2237eb6ea4f3111b4a43bb854e69890185043716aa3e'
